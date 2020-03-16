e4c5c202a927d98c436a08662724c253c0d7be66
Matheus Richard <matheusrichardt@gmail.com>
2020-03-16 00:05:52 -0300

    Add basic modules and desired API

--- src/dontfile.cr
+++ src/dontfile.cr
@@ -1,6 +1,6 @@
+require "./dontfile/cli"
+
 # TODO: Write documentation for `Dontfile`
 module Dontfile
   VERSION = "0.1.0"
-
-  # TODO: Put your code here
 end
---
+++ src/dontfile/api.cr
@@ -0,0 +1,5 @@
+require "http/client"
+
+class API
+  # api = HTTP::Client.new "www.dontfile.com"
+end
