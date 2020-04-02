6e64f1b798854a31ffc25926638d20d9277a6c21
Matheus Richard <matheusrichardt@gmail.com>
2020-04-01 21:47:33 -0300

    Add Page.from_path method

--- src/dontfile/page.cr
+++ src/dontfile/page.cr
@@ -4,5 +4,9 @@ module Dontfile
       url: String,
       content: String?,
     )
+
+    def self.from_path(path : String)
+      Dontfile::Client.new.page(path) # TODO: Maybe the client should be a singleton
+    end
   end
 end
