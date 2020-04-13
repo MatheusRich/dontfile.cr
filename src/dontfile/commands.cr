56892c9580902a5b919f0182a00961dec8e7fc11
Matheus Richard <matheusrichardt@gmail.com>
2020-04-13 19:59:02 -0300

    Add dontfile cli help

--- src/dontfile/commands.cr
+++ src/dontfile/commands.cr
@@ -1,4 +1,17 @@
 module Dontfile::Command
+  USAGE = <<-USAGE
+   Usage: dontfile [command] [options]
+
+   Command:
+       read                     outputs the content of a page
+       write                    writes on a page
+       help, --help, -h         shows this help
+       version, --version, -v   shows version
+
+   Run a command followed by --help to see command specific information. Ex:
+       dontfile <command> --help
+   USAGE
+
   def self.run(args = ARGV)
     command = args.first? || ""
 
@@ -13,6 +26,8 @@ module Dontfile::Command
       Dontfile::Command::WritePage.run(args)
     when "-v", "--version", "version"
       puts Dontfile::VERSION
+    when "-h", "--help", "help"
+      puts USAGE
     else
       Dontfile::Command::ReadPage.run(args)
     end
